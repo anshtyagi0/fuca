d41edcc54eb129ffdb5586ff74f3573248073dda
Ansh Tyagi <anshtyagi0093@gmail.com>
2023-06-11 18:07:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ module.exports={
     Developers: ["671390595184459782","836958855866089512"],
 
     Bot: {
-        ClientID: "1019613932211675237",
-        ClientSecret: "3SnDukUsERQTgPVSHVQhI1zr_niLs-qH",
-        ClientToken: "MTAxOTYxMzkzMjIxMTY3NTIzNw.GL1Bpn.B7yso61UDpVeKwXSJmHFsx8tT9e8msqlm9c8ZM",
+        ClientID: "ID",
+        ClientSecret: "SECRET",
+        ClientToken: "TOKEN",
         Color: "Blurple",
         BotPrefix: "c!"
     },
 
-    Mongoose: "mongodb+srv://ansh:0093@cosmic.9awu9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    Mongoose: "Mongo URL",
     Website: {
         port: 10000,
-        callbackURL: 'https://fuca.fluiddev.xyz/callback'
+        callbackURL: 'http://localhost:10000/callback'
         
     }
 }
